cdf56e9123940c6e82edee946001bbce98b96261
ramsr39 <ramesh86.java@gmail.com>
2014-12-25 23:31:57 +0530

    added columns for contact dto

--- src/main/java/com/apartment/management/dto/ContactDTO.java
+++ src/main/java/com/apartment/management/dto/ContactDTO.java
@@ -5,6 +5,10 @@ public final class ContactDTO {
 	private String emailId;
 	private String phoneNumber;
 	private String description;
+	private String type;
+	private String isVisableToPublic;
+	private String webSite;
+	private String name;
 	private Address address = new Address();
 
 	public String getId() {
@@ -37,6 +41,38 @@ public final class ContactDTO {
 
 	public void setDescription(final String description) {
 		this.description = description;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(final String type) {
+		this.type = type;
+	}
+
+	public String getIsVisableToPublic() {
+		return isVisableToPublic;
+	}
+
+	public void setIsVisableToPublic(final String isVisableToPublic) {
+		this.isVisableToPublic = isVisableToPublic;
+	}
+
+	public String getWebSite() {
+		return webSite;
+	}
+
+	public void setWebSite(final String webSite) {
+		this.webSite = webSite;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(final String name) {
+		this.name = name;
 	}
 
 	public Address getAddress() {
