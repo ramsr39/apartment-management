7439c25cd5bf2a776696ec2850b6293c2a8e37ea
ramsr39 <ramesh86.java@gmail.com>
2014-12-30 01:41:08 +0530

    updated format

--- src/main/java/com/apartment/management/utils/JsonUtils.java
+++ src/main/java/com/apartment/management/utils/JsonUtils.java
@@ -6,30 +6,31 @@ import com.google.gson.GsonBuilder;
 
 public class JsonUtils {
 
-	public static String parseObjectToJson(final Object obj) {
-		try{
-		Gson gson = new GsonBuilder().create();
-		return gson.toJson(obj);
-		}catch(Exception e){
-			throw new RuntimeException("unable to parse object to json::",e);
-		}
+  public static String parseObjectToJson(final Object obj) {
+    try {
+      Gson gson = new GsonBuilder().create();
+      return gson.toJson(obj);
+    } catch (Exception e) {
+      throw new RuntimeException("unable to parse object to json::", e);
+    }
 
-	}
+  }
 
-	public static <T> T parseJsonToObject(final String json, Class<T> clazz) {
-		try{
-		Gson gson = new GsonBuilder().create();
-		return gson.fromJson(json, clazz);
-		}catch(Exception e){
-			throw new RuntimeException("unable to parse json to object::"+json,e);
-		}
-	}
+  public static <T> T parseJsonToObject(final String json, Class<T> clazz) {
+    try {
+      Gson gson = new GsonBuilder().create();
+      return gson.fromJson(json, clazz);
+    } catch (Exception e) {
+      throw new RuntimeException("unable to parse json to object::" + json, e);
+    }
+  }
 
-	public static void main(String[] args) {
-		String payload = "{\"country\":\"India\",\"address1\":\"kphb phase9\",\"city\":\"vizag\",\"postalCode\":500054,\"state\":\"Andhra Pradesh\",\"name\":\"top building\",\"totalUnits\":\"11\",\"totalFloors\":\"11\"}";
-		BuildingDTO dto = parseJsonToObject(payload,BuildingDTO.class);
-		System.out.println(dto.getState());
-		
-	}
+  public static void main(String[] args) {
+    String payload =
+      "{\"country\":\"India\",\"address1\":\"kphb phase9\",\"city\":\"vizag\",\"postalCode\":500054,\"state\":\"Andhra Pradesh\",\"name\":\"top building\",\"totalUnits\":\"11\",\"totalFloors\":\"11\"}";
+    BuildingDTO dto = parseJsonToObject(payload, BuildingDTO.class);
+    System.out.println(dto.getState());
+
+  }
 
 }
