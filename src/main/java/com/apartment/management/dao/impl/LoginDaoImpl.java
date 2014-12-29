a7e99981e110ec5eeef26781fb31d65d3019f3c4
ramsr39 <ramesh86.java@gmail.com>
2014-12-30 00:11:19 +0530

    updated the format

--- src/main/java/com/apartment/management/dao/impl/LoginDaoImpl.java
+++ src/main/java/com/apartment/management/dao/impl/LoginDaoImpl.java
@@ -14,56 +14,52 @@ import com.apartment.management.dto.UserDTO;
 
 public class LoginDaoImpl extends SimpleJdbcDaoSupport implements LoginDao {
 
-	private static final Logger LOG = LoggerFactory
-			.getLogger(LoginDaoImpl.class);
+  private static final Logger LOG = LoggerFactory.getLogger(LoginDaoImpl.class);
 
-	private static String USER_QUERY = "SELECT count(EMAILID) FROM userinfo WHERE EMAILID=:EMAILID";
+  private static String USER_QUERY = "SELECT count(EMAILID) FROM userinfo WHERE EMAILID=:EMAILID";
 
-	private static String GET_USER_PWD_QUERY = "SELECT PASSWORD FROM userinfo WHERE EMAILID=:EMAILID";
+  private static String GET_USER_PWD_QUERY = "SELECT PASSWORD FROM userinfo WHERE EMAILID=:EMAILID";
 
-	private static String GET_USER_INFO_QUERY = "SELECT USERID,FIRSTNAME,LASTNAME,EMAILID,USERROLE FROM userinfo WHERE EMAILID=:EMAILID";
+  private static String GET_USER_INFO_QUERY =
+    "SELECT USERID,FIRSTNAME,LASTNAME,EMAILID,USERROLE FROM userinfo WHERE EMAILID=:EMAILID";
 
-	@Override
-	public boolean isUserExist(final String emailId) {
-		LOG.info("isUserExist started for the user:::::::::" + emailId);
-		MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
-		mapSqlParameterSource.addValue("EMAILID", emailId);
-		int count = getSimpleJdbcTemplate().queryForInt(USER_QUERY,
-				mapSqlParameterSource);
-		if (0 == count) {
-			return false;
-		}
-		LOG.info("isUserExist end");
-		return true;
-	}
+  @Override
+  public boolean isUserExist(final String emailId) {
+    LOG.info("isUserExist started for the user:::::::::" + emailId);
+    MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
+    mapSqlParameterSource.addValue("EMAILID", emailId);
+    int count = getSimpleJdbcTemplate().queryForInt(USER_QUERY, mapSqlParameterSource);
+    if (0 == count) {
+      return false;
+    }
+    LOG.info("isUserExist end");
+    return true;
+  }
 
-	@Override
-	public String getPasswordForUser(final String emailId) {
-		LOG.info("getPasswordForUser started for:::::::::" + emailId);
-		MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
-		mapSqlParameterSource.addValue("EMAILID", emailId);
-		return getSimpleJdbcTemplate().queryForObject(GET_USER_PWD_QUERY,
-				String.class, mapSqlParameterSource);
-	}
+  @Override
+  public String getPasswordForUser(final String emailId) {
+    LOG.info("getPasswordForUser started for:::::::::" + emailId);
+    MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
+    mapSqlParameterSource.addValue("EMAILID", emailId);
+    return getSimpleJdbcTemplate().queryForObject(GET_USER_PWD_QUERY, String.class, mapSqlParameterSource);
+  }
 
-	@Override
-	public UserDTO getUserDetails(final String userName) {
-		LOG.info("getUserDetails started for the user:::::::::" + userName);
-		MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
-		mapSqlParameterSource.addValue("EMAILID", userName);
-		return getSimpleJdbcTemplate().queryForObject(GET_USER_INFO_QUERY,
-				new RowMapper<UserDTO>() {
-					@Override
-					public UserDTO mapRow(final ResultSet rs, final int rowNum)
-							throws SQLException {
-						UserDTO dto = new UserDTO();
-						dto.setEmailId(rs.getString("EMAILID"));
-						dto.setRole(rs.getString("USERROLE"));
-						dto.setFirstName(rs.getString("FIRSTNAME"));
-						dto.setLastName(rs.getString("LASTNAME"));
-						return dto;
-					}
-				}, mapSqlParameterSource);
-	}
+  @Override
+  public UserDTO getUserDetails(final String userName) {
+    LOG.info("getUserDetails started for the user:::::::::" + userName);
+    MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
+    mapSqlParameterSource.addValue("EMAILID", userName);
+    return getSimpleJdbcTemplate().queryForObject(GET_USER_INFO_QUERY, new RowMapper<UserDTO>() {
+      @Override
+      public UserDTO mapRow(final ResultSet rs, final int rowNum) throws SQLException {
+        UserDTO dto = new UserDTO();
+        dto.setEmailId(rs.getString("EMAILID"));
+        dto.setRole(rs.getString("USERROLE"));
+        dto.setFirstName(rs.getString("FIRSTNAME"));
+        dto.setLastName(rs.getString("LASTNAME"));
+        return dto;
+      }
+    }, mapSqlParameterSource);
+  }
 
 }
