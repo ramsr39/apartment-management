b13ed3923e5fb84209a378ef86dddf066bf2a63e
ramsr39 <ramesh86.java@gmail.com>
2014-12-30 00:03:24 +0530

    updated the format

--- src/main/java/com/apartment/management/dao/impl/BuildingDaoImpl.java
+++ src/main/java/com/apartment/management/dao/impl/BuildingDaoImpl.java
@@ -36,7 +36,7 @@ public class BuildingDaoImpl extends NamedParameterJdbcDaoSupport
 			+ "COUNTRY,"
 			+ "STATE,"
 			+ "CITY,"
-		    + "PIN,"
+		       + "PIN,"
 			+ "OWNER_ID,"
 			+ "ADDRESS_LINE1,"
 			+ "ADDRESS_LINE2,"
@@ -81,147 +81,149 @@ public class BuildingDaoImpl extends NamedParameterJdbcDaoSupport
 
 	private static final String GET_BUILDING_OWNER_QUERY = "SELECT USERID,FIRSTNAME,LASTNAME,EMAILID,PRIMARY_PH_NO FROM userinfo WHERE EMAILID=:OWNEREID";
 	
-	private static final Logger LOG = LoggerFactory.getLogger(BuildingDaoImpl.class); 
+  private static final Logger LOG = LoggerFactory.getLogger(BuildingDaoImpl.class);
 
-	@Override
-	public String save(final BuildingDTO buildingDTO) {
-		final MapSqlParameterSource mapSqlParameterSource = prepareParamenterSource(buildingDTO);
-		final String buildingId="B"+RandomStringUtils.randomNumeric(8);
-		mapSqlParameterSource.addValue("BUILDING_ID", buildingId);
-	    getNamedParameterJdbcTemplate().update(INSERT_BUILDING_QUERY, mapSqlParameterSource);
-	    return buildingId;
-	}
+  @Override
+  public String save(final BuildingDTO buildingDTO) {
+    final MapSqlParameterSource mapSqlParameterSource = prepareParamenterSource(buildingDTO);
+    final String buildingId = "B" + RandomStringUtils.randomNumeric(8);
+    mapSqlParameterSource.addValue("BUILDING_ID", buildingId);
+    getNamedParameterJdbcTemplate().update(INSERT_BUILDING_QUERY, mapSqlParameterSource);
+    return buildingId;
+  }
 
-	@Override
-	public void update(final BuildingDTO buildingDTO) {
-		final MapSqlParameterSource mapSqlParameterSource = prepareParamenterSource(buildingDTO);
-		mapSqlParameterSource.addValue("BUILDING_ID", buildingDTO.getId());
-	    getNamedParameterJdbcTemplate().update(UPDATE_BUILDING_QUERY, mapSqlParameterSource);
-	}
+  @Override
+  public void update(final BuildingDTO buildingDTO) {
+    final MapSqlParameterSource mapSqlParameterSource = prepareParamenterSource(buildingDTO);
+    mapSqlParameterSource.addValue("BUILDING_ID", buildingDTO.getId());
+    getNamedParameterJdbcTemplate().update(UPDATE_BUILDING_QUERY, mapSqlParameterSource);
+  }
 
-	@Override
-	public void delete(final long buiildingId) {
-		final MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
-		mapSqlParameterSource.addValue("BUILDING_ID", buiildingId);
-		getNamedParameterJdbcTemplate().update(DELETE_BUILDING_QUERY, mapSqlParameterSource);
-	}
+  @Override
+  public void delete(final long buiildingId) {
+    final MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
+    mapSqlParameterSource.addValue("BUILDING_ID", buiildingId);
+    getNamedParameterJdbcTemplate().update(DELETE_BUILDING_QUERY, mapSqlParameterSource);
+  }
 
-	@Override
-	public List<BuildingDTO> findBuildingDetailsByCommunityId(final String communityId) {
-		final MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
-		mapSqlParameterSource.addValue("COMMUNITY_ID", communityId);
-		return getNamedParameterJdbcTemplate().query(GET_BUILDING_DETAILS_QUERY, mapSqlParameterSource,
-				new RowMapper<BuildingDTO>() {
-					@Override
-					public BuildingDTO mapRow(final ResultSet rs,final int rowNum) throws SQLException {
-						BuildingDTO buildingDto = new BuildingDTO();
-						buildingDto.setId(rs.getString("BUILDING_ID"));
-						buildingDto.setName(rs.getString("BUILDINGNAME"));
-						buildingDto.setTotalFloors(rs.getInt("NOOFFLOORS"));
-						buildingDto.setTotalUnits(rs.getInt("NOOFUNITS"));
-						buildingDto.setAddress1(rs.getString("ADDRESS_LINE1"));
-						buildingDto.setAddress2(rs.getString("ADDRESS_LINE2"));
-						buildingDto.setAddress3(rs.getString("ADDRESS_LINE3"));
-						buildingDto.setCity(rs.getString("CITY"));
-						buildingDto.setState(rs.getString("STATE"));
-						buildingDto.setCountry(rs.getString("COUNTRY"));
-						buildingDto.setPostalCode(rs.getInt("PIN"));
-						buildingDto.setImageUrl(rs.getString("IMAGE_URL"));
-						String ownerId = rs.getString("OWNER_ID");
-						if (null != ownerId) {
-							buildingDto.setOwnerDetails(getBuildingOwnerDetails(ownerId));
-						}
-						return buildingDto;
-					}
-				});
-	}
+  @Override
+  public List<BuildingDTO> findBuildingDetailsByCommunityId(final String communityId) {
+    final MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
+    mapSqlParameterSource.addValue("COMMUNITY_ID", communityId);
+    return getNamedParameterJdbcTemplate().query(GET_BUILDING_DETAILS_QUERY, mapSqlParameterSource,
+        new RowMapper<BuildingDTO>() {
+          @Override
+          public BuildingDTO mapRow(final ResultSet rs, final int rowNum) throws SQLException {
+            BuildingDTO buildingDto = new BuildingDTO();
+            buildingDto.setId(rs.getString("BUILDING_ID"));
+            buildingDto.setName(rs.getString("BUILDINGNAME"));
+            buildingDto.setTotalFloors(rs.getInt("NOOFFLOORS"));
+            buildingDto.setTotalUnits(rs.getInt("NOOFUNITS"));
+            buildingDto.setAddress1(rs.getString("ADDRESS_LINE1"));
+            buildingDto.setAddress2(rs.getString("ADDRESS_LINE2"));
+            buildingDto.setAddress3(rs.getString("ADDRESS_LINE3"));
+            buildingDto.setCity(rs.getString("CITY"));
+            buildingDto.setState(rs.getString("STATE"));
+            buildingDto.setCountry(rs.getString("COUNTRY"));
+            buildingDto.setPostalCode(rs.getInt("PIN"));
+            buildingDto.setImageUrl(rs.getString("IMAGE_URL"));
+            String ownerId = rs.getString("OWNER_ID");
+            if (null != ownerId) {
+              buildingDto.setOwnerDetails(getBuildingOwnerDetails(ownerId));
+            }
+            return buildingDto;
+          }
+        });
+  }
 
-	@Override
-	public BuildingDTO getBuildingDetailsByBuildingId(final String buildingId) {
-		final MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
-		mapSqlParameterSource.addValue("BUILDING_ID", buildingId);
-		return getNamedParameterJdbcTemplate().queryForObject(FIND_BUILDING_DETAILS_BY_BUILDINGID_QUERY, mapSqlParameterSource,
-				new RowMapper<BuildingDTO>() {
-					@Override
-					public BuildingDTO mapRow(final ResultSet rs,final int rowNum) throws SQLException {
-						BuildingDTO buildingDto = new BuildingDTO();
-						buildingDto.setId(rs.getString("BUILDING_ID"));
-						buildingDto.setCommunityId(rs.getString("COMMUNITY_ID"));
-						buildingDto.setName(rs.getString("BUILDINGNAME"));
-						buildingDto.setTotalFloors(rs.getInt("NOOFFLOORS"));
-						buildingDto.setTotalUnits(rs.getInt("NOOFUNITS"));
-						buildingDto.setAddress1(rs.getString("ADDRESS_LINE1"));
-						buildingDto.setAddress2(rs.getString("ADDRESS_LINE2"));
-						buildingDto.setAddress3(rs.getString("ADDRESS_LINE3"));
-						buildingDto.setCity(rs.getString("CITY"));
-						buildingDto.setState(rs.getString("STATE"));
-						buildingDto.setCountry(rs.getString("COUNTRY"));
-						buildingDto.setPostalCode(rs.getInt("PIN"));
-						buildingDto.setImageUrl(rs.getString("IMAGE_URL"));
-						return buildingDto;
-					}
-				});
-	}
+  @Override
+  public BuildingDTO getBuildingDetailsByBuildingId(final String buildingId) {
+    final MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
+    mapSqlParameterSource.addValue("BUILDING_ID", buildingId);
+    return getNamedParameterJdbcTemplate().queryForObject(FIND_BUILDING_DETAILS_BY_BUILDINGID_QUERY,
+        mapSqlParameterSource, new RowMapper<BuildingDTO>() {
+          @Override
+          public BuildingDTO mapRow(final ResultSet rs, final int rowNum) throws SQLException {
+            BuildingDTO buildingDto = new BuildingDTO();
+            buildingDto.setId(rs.getString("BUILDING_ID"));
+            buildingDto.setCommunityId(rs.getString("COMMUNITY_ID"));
+            buildingDto.setName(rs.getString("BUILDINGNAME"));
+            buildingDto.setTotalFloors(rs.getInt("NOOFFLOORS"));
+            buildingDto.setTotalUnits(rs.getInt("NOOFUNITS"));
+            buildingDto.setAddress1(rs.getString("ADDRESS_LINE1"));
+            buildingDto.setAddress2(rs.getString("ADDRESS_LINE2"));
+            buildingDto.setAddress3(rs.getString("ADDRESS_LINE3"));
+            buildingDto.setCity(rs.getString("CITY"));
+            buildingDto.setState(rs.getString("STATE"));
+            buildingDto.setCountry(rs.getString("COUNTRY"));
+            buildingDto.setPostalCode(rs.getInt("PIN"));
+            buildingDto.setImageUrl(rs.getString("IMAGE_URL"));
+            return buildingDto;
+          }
+        });
+  }
 
-	@Override
-	public String getBuildingNameById(final String buildingId) {
-		final MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
-		mapSqlParameterSource.addValue("BUILDING_ID", buildingId);
-		return getNamedParameterJdbcTemplate().queryForObject(GET_BUILDINGNAME_BY_ID, mapSqlParameterSource, String.class);
-	}
+  @Override
+  public String getBuildingNameById(final String buildingId) {
+    final MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
+    mapSqlParameterSource.addValue("BUILDING_ID", buildingId);
+    return getNamedParameterJdbcTemplate().queryForObject(GET_BUILDINGNAME_BY_ID, mapSqlParameterSource, String.class);
+  }
 
-	@Override
-	public Map<String, String> getBuildingCommunityById(final String buildingId) {
-		final MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
-		final Map<String, String> communityMap = new HashMap<String, String>();
-		mapSqlParameterSource.addValue("BUILDING_ID", buildingId);
-		final String communityId = getNamedParameterJdbcTemplate().queryForObject(GET_COMMUNITYID_FOR_BUILDING_QUERY, mapSqlParameterSource,String.class);
-		final String communityName = communityDetailsDao.getCommunityName(communityId);
-		communityMap.put("communityId", String.valueOf(communityId));
-		communityMap.put("communityName", communityName);
-		return communityMap;
-	}
+  @Override
+  public Map<String, String> getBuildingCommunityById(final String buildingId) {
+    final MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
+    final Map<String, String> communityMap = new HashMap<String, String>();
+    mapSqlParameterSource.addValue("BUILDING_ID", buildingId);
+    final String communityId =
+      getNamedParameterJdbcTemplate().queryForObject(GET_COMMUNITYID_FOR_BUILDING_QUERY, mapSqlParameterSource,
+          String.class);
+    final String communityName = communityDetailsDao.getCommunityName(communityId);
+    communityMap.put("communityId", String.valueOf(communityId));
+    communityMap.put("communityName", communityName);
+    return communityMap;
+  }
 
-	private MapSqlParameterSource prepareParamenterSource(final BuildingDTO buildingDTO) {
-		final MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
-		mapSqlParameterSource.addValue("COMMUNITY_ID", buildingDTO.getCommunityId());
-		mapSqlParameterSource.addValue("BUILDINGNAME", buildingDTO.getName());
-		mapSqlParameterSource.addValue("NOOFUNITS", buildingDTO.getTotalUnits());
-		mapSqlParameterSource.addValue("NOOFFLOORS", buildingDTO.getTotalFloors());
-		mapSqlParameterSource.addValue("ADDRESS_LINE1", buildingDTO.getAddress1());
-		mapSqlParameterSource.addValue("ADDRESS_LINE2", buildingDTO.getAddress2());
-		mapSqlParameterSource.addValue("ADDRESS_LINE3", buildingDTO.getAddress3());
-		mapSqlParameterSource.addValue("COUNTRY", buildingDTO.getCountry());
-		mapSqlParameterSource.addValue("STATE", buildingDTO.getState());
-		mapSqlParameterSource.addValue("CITY", buildingDTO.getCity());
-		mapSqlParameterSource.addValue("PIN", buildingDTO.getPostalCode());
-		mapSqlParameterSource.addValue("IMAGE_URL", buildingDTO.getImageUrl());
-		mapSqlParameterSource.addValue("OWNER_ID", buildingDTO.getOwnerDetails().getEmailId());
-		return mapSqlParameterSource;
-	}
+  private MapSqlParameterSource prepareParamenterSource(final BuildingDTO buildingDTO) {
+    final MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
+    mapSqlParameterSource.addValue("COMMUNITY_ID", buildingDTO.getCommunityId());
+    mapSqlParameterSource.addValue("BUILDINGNAME", buildingDTO.getName());
+    mapSqlParameterSource.addValue("NOOFUNITS", buildingDTO.getTotalUnits());
+    mapSqlParameterSource.addValue("NOOFFLOORS", buildingDTO.getTotalFloors());
+    mapSqlParameterSource.addValue("ADDRESS_LINE1", buildingDTO.getAddress1());
+    mapSqlParameterSource.addValue("ADDRESS_LINE2", buildingDTO.getAddress2());
+    mapSqlParameterSource.addValue("ADDRESS_LINE3", buildingDTO.getAddress3());
+    mapSqlParameterSource.addValue("COUNTRY", buildingDTO.getCountry());
+    mapSqlParameterSource.addValue("STATE", buildingDTO.getState());
+    mapSqlParameterSource.addValue("CITY", buildingDTO.getCity());
+    mapSqlParameterSource.addValue("PIN", buildingDTO.getPostalCode());
+    mapSqlParameterSource.addValue("IMAGE_URL", buildingDTO.getImageUrl());
+    mapSqlParameterSource.addValue("OWNER_ID", buildingDTO.getOwnerDetails().getEmailId());
+    return mapSqlParameterSource;
+  }
 
-	private UserDTO getBuildingOwnerDetails(final String ownerId) {
-		MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
-		mapSqlParameterSource.addValue("OWNERID", ownerId);
-		return getNamedParameterJdbcTemplate().queryForObject(GET_BUILDING_OWNER_QUERY, mapSqlParameterSource, new RowMapper<UserDTO>() {
-			@Override
-			public UserDTO mapRow(final ResultSet rs,final int rowNum) throws SQLException {
-				LOG.info("FlatDaoImpl getOwnerDetailsForFalt mapRow ::::start");
-				UserDTO userDTO = new UserDTO();
-				userDTO.setUserId(rs.getString("USERID"));
-				userDTO.setFirstName(rs.getString("FIRSTNAME"));
-				userDTO.setLastName(rs.getString("LASTNAME"));
-				userDTO.setEmailId(rs.getString("EMAILID"));
-				userDTO.setPrimaryPhoneNumber(rs.getString("PRIMARY_PH_NO"));
-				LOG.info("FlatDaoImpl getOwnerDetailsForFalt mapRow ::::end");
-				return userDTO;
-			}
-		});
-	}
+  private UserDTO getBuildingOwnerDetails(final String ownerId) {
+    MapSqlParameterSource mapSqlParameterSource = new MapSqlParameterSource();
+    mapSqlParameterSource.addValue("OWNERID", ownerId);
+    return getNamedParameterJdbcTemplate().queryForObject(GET_BUILDING_OWNER_QUERY, mapSqlParameterSource,
+        new RowMapper<UserDTO>() {
+          @Override
+          public UserDTO mapRow(final ResultSet rs, final int rowNum) throws SQLException {
+            LOG.info("FlatDaoImpl getOwnerDetailsForFalt mapRow ::::start");
+            UserDTO userDTO = new UserDTO();
+            userDTO.setUserId(rs.getString("USERID"));
+            userDTO.setFirstName(rs.getString("FIRSTNAME"));
+            userDTO.setLastName(rs.getString("LASTNAME"));
+            userDTO.setEmailId(rs.getString("EMAILID"));
+            userDTO.setPrimaryPhoneNumber(rs.getString("PRIMARY_PH_NO"));
+            LOG.info("FlatDaoImpl getOwnerDetailsForFalt mapRow ::::end");
+            return userDTO;
+          }
+        });
+  }
 
-	public void setCommunityDetailsDao(final CommunityDetailsDao communityDetailsDao) {
-		this.communityDetailsDao = communityDetailsDao;
-	}
-
+  public void setCommunityDetailsDao(final CommunityDetailsDao communityDetailsDao) {
+    this.communityDetailsDao = communityDetailsDao;
+  }
 
 }
