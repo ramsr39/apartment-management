91f9850744744e8f225d9ef95bd56074d5e06352
ramsr39 <ramesh86.java@gmail.com>
2015-01-20 00:15:01 +0530

    Added service to get pending list

--- src/main/java/com/apartment/management/services/ManageUserService.java
+++ src/main/java/com/apartment/management/services/ManageUserService.java
@@ -19,17 +19,30 @@ import javax.ws.rs.core.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.apartment.management.dao.AppointmentDao;
+import com.apartment.management.dao.ContactDao;
 import com.apartment.management.dao.ManageUserDao;
+import com.apartment.management.dao.UtilityDao;
+import com.apartment.management.dto.AppointmentDTO;
 import com.apartment.management.dto.CoOccupantDTO;
+import com.apartment.management.dto.ContactDTO;
 import com.apartment.management.dto.UserDTO;
 import com.apartment.management.dto.UserPrivilegeDTO;
+import com.apartment.management.dto.UtilityDTO;
 import com.apartment.management.utils.JsonUtils;
 
 @Path("/users")
 public class ManageUserService {
 
   private static final Logger LOG = LoggerFactory.getLogger(ManageUserService.class);
+
+  private ContactDao contactDao;
+
   private ManageUserDao manageUserDao;
+
+  private UtilityDao utilityDao;
+
+  private AppointmentDao appointmentDao;
 
   @POST
   @Consumes(MediaType.APPLICATION_JSON)
@@ -116,7 +129,8 @@ public class ManageUserService {
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
   @Path("/add-role")
-  public Response addRole(final String payload,@HeaderParam("userId") final String userId) {
+  public Response addRole(final String payload, @HeaderParam("userId")
+  final String userId) {
     UserPrivilegeDTO userPrivilegeDTO = JsonUtils.parseJsonToObject(payload, UserPrivilegeDTO.class);
     if (null == userPrivilegeDTO) {
       throw new RuntimeException("unable to parse user information");
@@ -131,7 +145,8 @@ public class ManageUserService {
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
   @Path("/update-role")
-  public Response updateRole(final String payload,@HeaderParam("userId") final String userId) {
+  public Response updateRole(final String payload, @HeaderParam("userId")
+  final String userId) {
     LOG.info("updateRole started...............");
     UserPrivilegeDTO userPrivilegeDTO = JsonUtils.parseJsonToObject(payload, UserPrivilegeDTO.class);
     if (null == userPrivilegeDTO) {
@@ -139,52 +154,95 @@ public class ManageUserService {
     }
     userPrivilegeDTO.setUpdatedBy(userId);
     final String managementGroupId = manageUserDao.updateRole(userPrivilegeDTO);
-    LOG.info("successfully updateRole for the id..............."+managementGroupId);
+    LOG.info("successfully updateRole for the id..............." + managementGroupId);
     return Response.ok().entity(JsonUtils.parseObjectToJson(userPrivilegeDTO)).build();
   }
 
   @PUT
   @Path("/dalete-role/{managementGroupId}")
-  public Response deleteRole(final String payload,@HeaderParam("managementGroupId") final String managementGroupId) {
-    LOG.info("deleteRole started for id........"+managementGroupId);
-     manageUserDao.deleteRole(managementGroupId);
-    LOG.info("successfully deleted role for the id..............."+managementGroupId);
+  public Response deleteRole(final String payload, @HeaderParam("managementGroupId")
+  final String managementGroupId) {
+    LOG.info("deleteRole started for id........" + managementGroupId);
+    manageUserDao.deleteRole(managementGroupId);
+    LOG.info("successfully deleted role for the id..............." + managementGroupId);
     return Response.ok().build();
   }
-  
+
   @GET
   @Produces(MediaType.APPLICATION_JSON)
   @Path("/get-community-roles")
-  public String getCommiunityRoles(@QueryParam("communityId") final String communityId) {
-    LOG.info("getCommiunityRoles started for id........"+communityId);
+  public String getCommiunityRoles(@QueryParam("communityId")
+  final String communityId) {
+    LOG.info("getCommiunityRoles started for id........" + communityId);
     List<UserPrivilegeDTO> rolesList = manageUserDao.getCommunityRoles(communityId);
-    LOG.info("getCommiunityRoles end.for the id............."+communityId);
+    LOG.info("getCommiunityRoles end.for the id............." + communityId);
     return JsonUtils.parseObjectToJson(rolesList);
   }
 
   @GET
   @Produces(MediaType.APPLICATION_JSON)
   @Path("/get-user-roles")
-  public String getUserRoles(@QueryParam("userId") final String userId) {
-    LOG.info("getUserRoles started for id........"+userId);
-    List<UserPrivilegeDTO> rolesList =  manageUserDao.getUserRoles(userId);
-    LOG.info("getUserRoles end.for the id............."+userId);
+  public String getUserRoles(@QueryParam("userId")
+  final String userId) {
+    LOG.info("getUserRoles started for id........" + userId);
+    List<UserPrivilegeDTO> rolesList = manageUserDao.getUserRoles(userId);
+    LOG.info("getUserRoles end.for the id............." + userId);
     return JsonUtils.parseObjectToJson(rolesList);
   }
-
 
   @GET
   @Produces(MediaType.APPLICATION_JSON)
   @Path("/check-user-residency-in-community")
-  public String isUserResidentInCommunity(@QueryParam("communityId") final String communityId,@QueryParam("userId") final String userId) {
-    LOG.info("getUserRoles started for id........"+userId);
-    boolean flag = manageUserDao.isUserResidentInCommunity(communityId,userId);
-    LOG.info("getUserRoles end.for the id............."+userId);
+  public String isUserResidentInCommunity(@QueryParam("communityId")
+  final String communityId, @QueryParam("userId")
+  final String userId) {
+    LOG.info("isUserResidentInCommunity started for id........" + communityId);
+    boolean flag = manageUserDao.isUserResidentInCommunity(communityId, userId);
+    LOG.info("isUserResidentInCommunity end.for the id............." + communityId);
     return String.valueOf(flag);
   }
- 
+
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Path("/get-user-pending-items")
+  public String getUserPendingItems(@QueryParam("userId")
+  final String userId, final String payload) {
+    LOG.info("getUserPendingItems started for id........" + userId);
+    UserPrivilegeDTO userPrivilegeDTO = JsonUtils.parseJsonToObject(payload, UserPrivilegeDTO.class);
+    if (userPrivilegeDTO.isApproveOthersUtilities()) {
+        List<UtilityDTO> pendingUtilities = utilityDao.findPendingUtilities(userId,userPrivilegeDTO.getCommunityId());
+    }
+    if (userPrivilegeDTO.isApproveOthersContacts()) {
+      List<ContactDTO> pendingUtilities = contactDao.findPendingContacts(userId,userPrivilegeDTO.getCommunityId());
+    }
+    if (userPrivilegeDTO.isApproveOthersAppointments()) {
+      List<AppointmentDTO> pendingUtilities = appointmentDao.findPendingAppointments(userId,userPrivilegeDTO.getCommunityId());
+    }
+    if (userPrivilegeDTO.isApproveOwnNotices()) {
+
+    }
+    if (userPrivilegeDTO.isApproveOthersExpenses()) {
+
+    }
+    LOG.info("getUserPendingItems end.for the id............." + userId);
+    return null;
+  }
+
   public void setManageUserDao(final ManageUserDao manageUserDao) {
     this.manageUserDao = manageUserDao;
+  }
+
+  public void setContactDao(final ContactDao contactDao) {
+    this.contactDao = contactDao;
+  }
+
+  public void setUtilityDao(final UtilityDao utilityDao) {
+    this.utilityDao = utilityDao;
+  }
+
+  public void setAppointmentDao(final AppointmentDao appointmentDao) {
+    this.appointmentDao = appointmentDao;
   }
 
 }
